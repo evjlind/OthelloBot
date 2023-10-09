9099616a40543bf075361360ada2a42bfb3d8801
evjlind <evanjlindeman@gwu.edu>
2023-10-09 16:53:50 -0400

    cleanup + human vs agent play

--- TryToLose/play_agent.py
+++ TryToLose/play_agent.py
@@ -12,22 +12,33 @@ print(board)
 
 while ttt.playing(board):
     if turn == human:
-        row = int(input("human move row"))
-        col = int(input("human move col"))
-        board[row][col] = human
-        turn = -turn
+        selecting = True
+        while selecting:
+            moves = ttt.get_all_moves(board)
+            print(moves)
+            index = int(input("select index of move"))
+            if 0 <= index and index <= len(moves):
+                board[moves[index][0]][moves[index][1]] = human
+                turn = -turn
+                selecting = False
+            else:
+                print("select a valid move")
         print(board)
     else:
         all_moves = ttt.get_all_moves(board)
         scores = np.empty(len(all_moves))
         for move in range(len(all_moves)):
-            print(all_moves[move])
             scores[move] =  agent.score_move(board,all_moves[move],10,turn,-math.inf,math.inf)
-        print(scores)
         if turn == 1:
             index = np.where(scores == np.max(scores))
         else:
             index = np.where(scores == np.min(scores))
         board[all_moves[index[0][0]][0]][all_moves[index[0][0]][1]] = turn
         turn = -turn
-        print(board)
+
+if ttt.score(board) == human:
+    print("Human wins!")
+elif ttt.score(board) == -human:
+    print("Agent wins")
+else:
+    print("Draw")
