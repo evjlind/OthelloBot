bd59646636fdcb58c707ae13accd8ae5ebe0b18e
evjlind <evanjlindeman@gwu.edu>
2024-01-09 15:42:04 -0500

    started othello C implementation

---
+++ Othello_C/othello.h
@@ -0,0 +1,9 @@
+#define INIT_BOARD_BLACK 0x810000000ULL
+#define INIT_BOARD_WHITE 0x1008000000ULL
+
+// #define INIT_BOARD_BLACK 0x810000000
+// #define INIT_BOARD_WHITE 0x1008000000
+
+unsigned long long get_valid_moves(int player, int black, int white){
+
+}
