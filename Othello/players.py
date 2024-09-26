fa6ac540a719bdab7d384d1e5cf51f059e85b076
evjlind <evanjlindeman@gwu.edu>
2024-09-26 11:52:11 -0400

    minor mistake in handmade.board_to_ints

--- Othello/players.py
+++ Othello/players.py
@@ -199,13 +199,16 @@ class HandMade():
 
     def board_to_ints(board):
         n_board = board.flatten()
+        print(len(n_board))
         p1_value = 0
         p2_value = 0
         for i in range(len(n_board)):
             if n_board[i] == 1:
-               p1_value += (64-i)**2
+               print(i)
+               p1_value += 2**(63-i)
             if n_board[i] == -1:
-               p2_value += (64-i)**2
+               print(i)
+               p2_value += 2**(63-i)
         return p1_value,p2_value
         
     # might not end up using this 
